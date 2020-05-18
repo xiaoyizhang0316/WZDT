10006bc23ca3566b60908a74cfcd9f9f0b46cf73
DESKTOP-IT25P84\Yseer <Yseerd@outlook.com>
2020-05-18 14:42:54 +0800

    no message

--- Assets/Old/Scripts/RoleManager/MapRole/Skill/PruductDealer.cs
+++ Assets/Old/Scripts/RoleManager/MapRole/Skill/PruductDealer.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using DT.Fight.Bullet;
 using UnityEngine;
 
 public class PruductDealer : BaseSkill
@@ -20,7 +21,23 @@ public class PruductDealer : BaseSkill
             //Debug.Log("攻击");
             ProductData data = role.warehouse[0];
             role.warehouse.RemoveAt(0);
-           GetComponent<BulletLaunch>().LanchNormal( data);
+            switch (data.bulletType)
+            {
+                case  BulletType.NormalPP: 
+                    GetComponent<BulletLaunch>().LanchNormal( data);
+                    break;    
+                case  BulletType.Bomb: 
+                    GetComponent<BulletLaunch>().LanchBoom( data);
+                    break;    
+                case  BulletType.Lightning: 
+                    GetComponent<BulletLaunch>().LanchNormal( data);
+                    break;    
+                case  BulletType.summon: 
+                    GetComponent<BulletLaunch>().LanchNormal( data);
+                    break;
+                 
+            }
+       
         
         }
     }
