c3be88e846b2d73fa0df60da5233479b29799cdf
Xiaoyi Zhang <517829767@qq.com>
2020-06-04 11:30:39 +0800

    no message

--- Assets/DT/Scripts/IOSystem/GameData/DataModel/StageData/StageItem.cs
+++ Assets/DT/Scripts/IOSystem/GameData/DataModel/StageData/StageItem.cs
@@ -20,6 +20,36 @@ public class StageItem
     public string startPlayerHealth;
 
     public string startPlayerGold;
+
+    /// <summary>
+    /// 1星奖励装备
+    /// </summary>
+    public string starOneEquip;
+
+    /// <summary>
+    /// 1星奖励工人
+    /// </summary>
+    public string starOneWorker;
+
+    /// <summary>
+    /// 2星奖励装备
+    /// </summary>
+    public string starTwoEquip;
+
+    /// <summary>
+    /// 2星奖励工人
+    /// </summary>
+    public string starTwoWorker;
+
+    /// <summary>
+    /// 3星奖励装备
+    /// </summary>
+    public string starThreeEquip;
+
+    /// <summary>
+    /// 3星奖励工人
+    /// </summary>
+    public string starThreeWorker;
 }
 
 public class StagesData
