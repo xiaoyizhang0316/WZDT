fc3ed9197448ac6f0d766669db3066484b94b7ac
wenjie <280592199@qq.com>
2020-06-15 12:05:17 +0800

    no message

---
+++ Assets/DT/Scripts/UI/DataStat/StatItem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatItem : MonoBehaviour
+{
+    public Text itemName;
+    public Text itemPerMinAndTotal;
+    //public Text itemTotal;
+
+    public void Setup(string name, int perMin, int total)
+    {
+        itemName.text = name;
+        itemPerMinAndTotal.text = string.Format($"{ perMin.ToString()}\t\t{total.ToString()}");
+        //itemTotal.text = total.ToString();
+
+        if (!gameObject.activeInHierarchy)
+        {
+            gameObject.SetActive(true);
+        }
+    }
+}
