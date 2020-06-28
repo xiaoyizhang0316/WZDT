1e44600918509dd32763d16a8797e6052a42e8a5
wenjie <280592199@qq.com>
2020-06-28 17:41:20 +0800

    no message

--- Assets/DT/Scripts/Info/NPCListInfo/NpcSpecialInfo.cs
+++ Assets/DT/Scripts/Info/NPCListInfo/NpcSpecialInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using DT.Fight.Bullet;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -21,7 +22,18 @@ public class NpcSpecialInfo : MonoBehaviour
 
     public Text level;
 
-    public void SetInfo(BaseMapRole npc, BaseSkill baseSkill)
+    public GameObject bulletWarehourse;
+
+    public Transform bulletContent;
+
+    public GameObject bulletPrefab;
+
+    public Sprite AOE;
+    public Sprite normallpp;
+    public Sprite lightning;
+    public Sprite tow;
+
+    public void SetInfo(BaseMapRole npc, BaseSkill baseSkill, Transform npcTF)
     {
         npcName.text = npc.baseRoleData.baseRoleData.roleName;
         skillDes.text = baseSkill.skillDesc;
@@ -31,28 +43,37 @@ public class NpcSpecialInfo : MonoBehaviour
         risk.text = npc.baseRoleData.riskResistance.ToString();
         level.text = npc.baseRoleData.baseRoleData.level.ToString();
         HideAll();
+        //ClearBulletContent();
         switch (npc.baseRoleData.baseRoleData.roleType)
         {
             case GameEnum.RoleType.Seed:
                 seedProp.transform.GetChild(0).GetChild(2).GetComponent<Text>().text = (npc.baseRoleData.efficiency / 20f).ToString("#.#") + "/s";
                 seedProp.transform.GetChild(1).GetChild(2).GetComponent<Text>().text = (npc.baseRoleData.effect*10).ToString();
                 seedProp.SetActive(true);
+                ShowSeedBullet(npcTF);
+                bulletWarehourse.SetActive(true);
                 break;
             case GameEnum.RoleType.Peasant:
                 peasantProp.transform.GetChild(0).GetChild(2).GetComponent<Text>().text = (npc.baseRoleData.efficiency / 10f).ToString("#.#") + "/s";
                 peasantProp.transform.GetChild(1).GetChild(2).GetComponent<Text>().text = (npc.baseRoleData.effect).ToString()+"%";
                 peasantProp.SetActive(true);
+                ShowPeasantBullet(npcTF);
+                bulletWarehourse.SetActive(true);
                 break;
             case GameEnum.RoleType.Merchant:
                 merchantProp.transform.GetChild(0).GetChild(2).GetComponent<Text>().text = (((npc.baseRoleData.efficiency * 0.3f) / 100f) * npc.baseRoleData.tradeCost).ToString("#.#");
                 merchantProp.transform.GetChild(1).GetChild(2).GetComponent<Text>().text = (npc.baseRoleData.effect).ToString() + "%";
                 merchantProp.SetActive(true);
+                ShowMerchantBullet(npcTF);
+                bulletWarehourse.SetActive(true);
                 break;
             case GameEnum.RoleType.Dealer:
                 dealerProp.transform.GetChild(0).GetChild(2).GetComponent<Text>().text = (npc.baseRoleData.efficiency).ToString() + "%";
                 dealerProp.transform.GetChild(1).GetChild(2).GetComponent<Text>().text = (npc.baseRoleData.range).ToString();
 
                 dealerProp.SetActive(true);
+                ShowDealerBullet(npcTF);
+                bulletWarehourse.SetActive(true);
                 break;
         }
         int i = 0;
@@ -92,11 +113,118 @@ public class NpcSpecialInfo : MonoBehaviour
         
     }
 
+    void ShowSeedBullet(Transform npc)
+    {
+        int count = 9;
+        if (npc.GetComponent<ProductSeed>().productDatas.Count < 9)
+        {
+            count = npc.GetComponent<ProductSeed>().productDatas.Count;
+        }
+
+        for (int i = 1; i <= count; i++)
+        {
+            GameObject Pruductgame = Instantiate(bulletPrefab, bulletContent);
+            Pruductgame.GetComponent<NpcBulletSign>().currentProduct =
+                npc.GetComponent<ProductSeed>().productDatas[npc.GetComponent<ProductSeed>().productDatas.Count - i];
+            Pruductgame.GetComponent<Image>().sprite = RoleUpdateInfo.My.seedSpeed;
+        }
+    }
+
+    void ShowPeasantBullet(Transform npc)
+    {
+        int count = 9;
+        if (npc.GetComponent<ProductMelon>().productDatas.Count < 9)
+        {
+            count = npc.GetComponent<ProductMelon>().productDatas.Count;
+        }
+
+
+        for (int i = 1; i <= count; i++)
+        {
+            //Debug.Log(i+"||"+ baseMapRole.GetComponent<ProductMelon>().productDatas.Count);
+            GameObject Pruductgame = Instantiate(bulletPrefab, bulletContent);
+            Pruductgame.GetComponent<NpcBulletSign>().currentProduct =
+                npc.GetComponent<ProductMelon>().productDatas[npc.GetComponent<ProductMelon>().productDatas.Count - i];
+            Pruductgame.GetComponent<Image>().sprite = RoleUpdateInfo.My.normallpp;
+        }
+    }
+
+    void ShowMerchantBullet(Transform npc)
+    {
+        BaseMapRole baseMapRole = npc.GetComponent<BaseMapRole>();
+        for (int i = 0; i < baseMapRole.warehouse.Count; i++)
+        {
+
+            GameObject Pruductgame = Instantiate(bulletPrefab, bulletContent);
+            Pruductgame.GetComponent<NpcBulletSign>().currentProduct =
+                baseMapRole.warehouse[i];
+            switch (baseMapRole.warehouse[i].bulletType)
+            {
+                case BulletType.Bomb:
+                    Pruductgame.GetComponent<Image>().sprite = AOE;
+                    break;
+                case BulletType.NormalPP:
+                    Pruductgame.GetComponent<Image>().sprite = normallpp;
+                    break;
+
+                case BulletType.Lightning:
+                    Pruductgame.GetComponent<Image>().sprite = lightning;
+                    break;
+
+                case BulletType.summon:
+                    Pruductgame.GetComponent<Image>().sprite = tow;
+                    break;
+
+            }
+
+        }
+    }
+
+    void ShowDealerBullet(Transform npc)
+    {
+        BaseMapRole baseMapRole = npc.GetComponent<BaseMapRole>();
+        for (int i = 0; i < baseMapRole.warehouse.Count; i++)
+        {
+            GameObject Pruductgame = Instantiate(bulletPrefab, bulletContent);
+            Pruductgame.GetComponent<NpcBulletSign>().currentProduct =
+                baseMapRole.warehouse[i];
+            switch (baseMapRole.warehouse[i].bulletType)
+            {
+                case BulletType.Bomb:
+                    Pruductgame.GetComponent<Image>().sprite = AOE;
+                    break;
+                case BulletType.NormalPP:
+                    Pruductgame.GetComponent<Image>().sprite = normallpp;
+                    break;
+                case BulletType.Lightning:
+                    Pruductgame.GetComponent<Image>().sprite = lightning;
+                    break;
+                case BulletType.summon:
+                    Pruductgame.GetComponent<Image>().sprite = tow;
+                    break;
+            }
+        }
+    }
+
     void HideAll()
     {
         seedProp.SetActive(false);
         peasantProp.SetActive(false);
         merchantProp.SetActive(false);
         dealerProp.SetActive(false);
+    }
+
+    void ClearBulletContent()
+    {
+        for(int i=0; i< bulletContent.childCount; i++)
+        {
+            Destroy(bulletContent.GetChild(i).gameObject);
+        }
+    }
+
+    public void Reset()
+    {
+        ClearBulletContent();
+        bulletWarehourse.SetActive(false);
     }
 }
