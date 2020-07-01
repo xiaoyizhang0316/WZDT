849edef93a8c9d9ca1b9b1fe230c3c1a9ffc0128
DESKTOP-IT25P84\Yseer <Yseerd@outlook.com>
2020-07-01 13:37:01 +0800

    no message

---
+++ Assets/DT/Scripts/UI/Review/ReviewRoleSign.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReviewRoleSign : MonoBehaviour
+{
+    public ReviewPanel.ReviewRole role;
+
+    void Awake()
+    {
+        ReviewManager.My.signs.Add(this);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
