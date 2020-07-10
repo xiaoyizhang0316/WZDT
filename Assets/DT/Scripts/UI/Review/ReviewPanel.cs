f75b7ad7f8ab10be4df6337571f15f08f8a7b0b1
Xiaoyi Zhang <517829767@qq.com>
2020-07-10 18:50:43 +0800

    no message

--- Assets/DT/Scripts/UI/Review/ReviewPanel.cs
+++ Assets/DT/Scripts/UI/Review/ReviewPanel.cs
@@ -121,20 +121,18 @@ public class ReviewPanel : MonoSingleton<ReviewPanel>
     {
         if (datas.Count == 0)
             return;
-        int maxAmount = datas[0].restMoney * 150 / 100;
+        int maxAmount = datas[0].restMoney;
+        for (int i = 0; i < datas.Count; i++)
+        {
+            if (datas[i].restMoney > maxAmount)
+                maxAmount = datas[i].restMoney;
+        }
         line.vectorLine.points2.Clear();
         print(timeCount);
         print(datas.Count);
         for (int i = 0; i < datas.Count; i++)
         {
-            if (datas[i].restMoney <= maxAmount)
-            {
-                line.vectorLine.points2.Add(new Vector2(1326f / (float)timeCount * 5f * i, datas[i].restMoney / (float)maxAmount * 100f));
-            }
-            else
-            {
-                line.vectorLine.points2.Add(new Vector2(1326f / (float)timeCount * 5f * i, 100f));
-            }
+            line.vectorLine.points2.Add(new Vector2(1326f / (float)timeCount * 5f * i, datas[i].restMoney / (float)maxAmount * 100f));
         }
         //line.vectorLine.points2.Add(new Vector2(1326f, Mathf.Min(datas[datas.Count - 1].restMoney / (float)maxAmount * 100f,100f)));
         line.vectorLine.Draw();
