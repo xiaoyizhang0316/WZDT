7aa5c81dd87af5ee1654d074b52a469e6a946b17
DESKTOP-IT25P84\Yseer <Yseerd@outlook.com>
2020-08-04 18:45:58 +0800

    no message

---
+++ Assets/DT/Scripts/Info/ComboManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ComboManager : IOIntensiveFramework.MonoSingleton.MonoSingleton<ComboManager>
+{
+    public Text num;
+
+    public GameObject combo;
+    public bool isShow;
+    public int combonum=0;
+    public float showTime=0;
+    public void AddComboNum()
+    {
+        combonum++;
+        num.text = combonum.ToString();
+        
+        combo.SetActive(true);
+      
+        
+
+        isShow = true;
+        showTime = 0;
+        num.transform.DOScale(1+combonum*0.01f, 0.4f).SetEase(Ease.InOutBounce).Play();
+    }
+
+    public void Update()
+    {
+        if (isShow)
+        {
+            showTime += Time.deltaTime;
+            if (showTime > 5)
+            {
+                isShow = false;
+                combo.SetActive(false);
+                combonum = 0;
+                num.transform.localScale = Vector3.one;
+            }
+        }
+    }
+
+    public void Start()
+    {
+        combo.SetActive(false);
+    }
+}
