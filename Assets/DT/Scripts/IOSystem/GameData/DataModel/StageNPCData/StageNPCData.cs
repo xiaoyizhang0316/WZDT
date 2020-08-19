4a1f02c9e6c9cb88c263aef9fd549121d618253e
Xiaoyi Zhang <517829767@qq.com>
2020-08-19 11:19:32 +0800

    no message

---
+++ Assets/DT/Scripts/IOSystem/GameData/DataModel/StageNPCData/StageNPCData.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageNPCData
+{
+    public string roleType;
+
+    public int level;
+
+    public string npcName;
+
+    public int effect;
+
+    public double npcID;
+
+    public int efficiency;
+
+    public int risk;
+
+    public int tradeCost;
+
+    public int range;
+
+    public int bulletCount;
+
+    public int posX;
+
+    public int posY;
+
+    public bool isCanSee;
+
+    public bool isLock;
+
+    public bool isCanSeeEquip;
+
+    public int lockNumber;
+
+    public List<int> initBuffList;
+
+    public List<int> hideBuffList;
+
+    public List<int> goodBaseBuffList;
+
+    public List<int> badBaseBuffList;
+}
---
+++ Assets/DT/Scripts/IOSystem/GameData/DataModel/StageNPCData/StageNPCItem.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageNPCItem
+{
+    public string roleType;
+
+    public string level;
+
+    public string npcName;
+
+    public string effect;
+
+    public string npcID;
+
+    public string efficiency;
+
+    public string risk;
+
+    public string tradeCost;
+
+    public string range;
+
+    public string bulletCount;
+
+    public string posX;
+
+    public string posY;
+
+    public string isCanSee;
+
+    public string isLock;
+
+    public string isCanSeeEquip;
+
+    public string lockNumber;
+
+    public string initBuffList;
+
+    public string hideBuffList;
+
+    public string goodBaseBuffList;
+
+    public string badBaseBuffList;
+}
