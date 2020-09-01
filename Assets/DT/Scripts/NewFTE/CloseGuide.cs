7ee4d77e0b631b98c4cfd68882ad213323498ecd
DESKTOP-IT25P84\Yseer <Yseerd@outlook.com>
2020-09-01 17:54:21 +0800

    no message

---
+++ Assets/DT/Scripts/NewFTE/CloseGuide.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CloseGuide : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (AnsweringPanel.My.isComplete)
+        {
+            gameObject.SetActive(false);
+        }
+
+        else
+        {
+          
+        }
+
+        GetComponent<Button>().onClick.AddListener(() =>
+        {
+            GuideManager.My.CloseFTE();
+            gameObject.SetActive(false);
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
