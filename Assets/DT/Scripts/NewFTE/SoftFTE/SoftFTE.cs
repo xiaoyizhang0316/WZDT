6d6e873e1955eb0d65141ac936aebbd49b191503
Xiaoyi Zhang <517829767@qq.com>
2020-09-08 17:27:15 +0800

    no message

---
+++ Assets/DT/Scripts/NewFTE/SoftFTE/SoftFTE.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using static GameEnum;
+
+public class SoftFTE : MonoSingleton<SoftFTE>
+{
+
+    public List<SoftFTE_Base> softList;
+
+    public int currentIndex;
+
+    public GameObject softFTEObj;
+
+    public void Init(RoleType type)
+    {
+        currentIndex = 0;
+        for (int i = 0; i < softList.Count; i++)
+        {
+            softList[i].Init(type);
+        }
+        SoftFTEStart();
+    }
+
+    public void PlayNext()
+    {
+        if (currentIndex == softList.Count)
+        {
+            SoftFTEEnd();
+        }
+        else
+        {
+            softList[currentIndex].gameObject.SetActive(true);
+            softList[currentIndex].Play();
+            currentIndex++;
+        }
+    }
+
+    public void SoftFTEStart()
+    {
+        NewCanvasUI.My.GamePause(false);
+        softFTEObj.SetActive(true);
+        CameraPlay.WidescreenH_ON(Color.black, 1);
+        Invoke("PlayNext", 1f);
+    }
+
+    public void SoftFTEEnd()
+    {
+        CameraPlay.WidescreenH_OFF();
+        transform.DOScale(transform.localScale, 1).OnComplete(() =>
+        {
+            NewCanvasUI.My.GameNormal();
+            softFTEObj.SetActive(false);
+        }).Play();
+    }
+
+    public void Test()
+    {
+        Init(RoleType.Bank);
+    }
+}
---
+++ Assets/DT/Scripts/NewFTE/SoftFTE/SoftFTE_Base.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using static GameEnum;
+
+public class SoftFTE_Base : MonoBehaviour
+{
+    public Button nextButton;
+
+    public float entryTime;
+
+    public virtual void Init(RoleType type)
+    {
+        nextButton.onClick.AddListener(() =>
+        {
+            NextStep();
+            gameObject.SetActive(false);
+        });
+        nextButton.interactable = false;
+    }
+
+    public void Play()
+    {
+        StartCoroutine(Check());
+    }
+
+    public virtual IEnumerator Check()
+    {
+        yield break;
+    }
+
+    public virtual void NextStep()
+    {
+        SoftFTE.My.PlayNext();
+    }
+    
+
+
+
+}
