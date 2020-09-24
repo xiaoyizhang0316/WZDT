20118d6182fcce661cd360ad29a59e4f05bad3eb
wqbeckham <39215806@qq.com>
2020-09-24 16:48:38 +0800

    Merge branch 'ZXY' of https://gitee.com/Yseerd/WZDT into wq
    # Conflicts:
    #	Assets/DT/Art/UI/UIPrb/Canvas_NEW.prefab
    #	Assets/DT/Scenes/FTE_4.unity
    #	Assets/DT/Scripts/Info/NPCListInfo/NpcServiceInfo.cs
    #	Assets/DT/Scripts/LevelSystem/LevelControll/Level7Controller.cs
    #	Assets/Easy Outline/Shaders/ScreenSpaceOutlineShader.mat
    #	Assets/StreamingAssets/build_info.txt

---
+++ Assets/DT/Scripts/UI/Role/EncourageLevel.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using static GameEnum;
+
+public class EncourageLevel : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler
+{
+    public BaseMapRole currentRole;
+
+    public Text levelText;
+
+    public string showStr;
+
+    public void Init(BaseMapRole role)
+    {
+        currentRole = role;
+        showStr = "激励等级0，无任何影响";
+        levelText.text = role.encourageLevel.ToString();
+        levelText.color = Color.white;
+        if (role.baseRoleData.baseRoleData.roleSkillType == RoleSkillType.Service)
+        {
+            if (role.encourageLevel > 0)
+            {
+                showStr = "激励等级" + role.encourageLevel.ToString() + " 角色的增益效果提高" + (role.encourageLevel * 10).ToString("##.##") +"%";
+                levelText.color = Color.green;
+            }
+            else if (role.encourageLevel < 0)
+            {
+                showStr = "激励等级" + role.encourageLevel.ToString() + " 角色的增益效果降低" + (role.encourageLevel * -10).ToString("##.##") + "%";
+                levelText.color = Color.red;
+            }
+        }
+        else
+        {
+            if (role.encourageLevel > 0)
+            {
+                showStr = "激励等级" + role.encourageLevel.ToString() + " 角色的生产速率提高" + (role.encourageLevel * 5).ToString("##.##") + "%";
+                levelText.color = Color.green;
+            }
+            else if (role.encourageLevel < 0)
+            {
+                showStr = "激励等级" + role.encourageLevel.ToString() + " 角色的生产速率降低" + (role.encourageLevel * -10).ToString("##.##") + "%";
+                levelText.color = Color.red;
+            }
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        FloatWindow.My.Init(showStr);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        FloatWindow.My.Hide();
+    }
+}
