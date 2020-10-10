6990c5d197c2ae41a1d708ecd49af2a4551ac701
Xiaoyi Zhang <517829767@qq.com>
2020-10-10 17:23:32 +0800

    no message

---
+++ Assets/DT/Scripts/Multi/TestMulti.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class TestMulti : NetworkBehaviour
+{
+    [ClientRpc]
+    public void RpcTest()
+    {
+        Debug.Log("dasds213124eadfadfdsf34321e2");
+        transform.position += Vector3.one;
+    }
+
+
+}
