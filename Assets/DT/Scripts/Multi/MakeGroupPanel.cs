b6752e2d779a334f9990c4e175a0135586ad234c
Xiaoyi Zhang <517829767@qq.com>
2020-10-22 19:28:41 +0800

    no message

---
+++ Assets/DT/Scripts/Multi/MakeGroupPanel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MakeGroupPanel : MonoSingleton<MakeGroupPanel>
+{
+
+    public Button makeGroupButton;
+
+    public Button joinButton;
+
+    public InputField ip;
+
+    public InputField port;
+
+    public InputField groupName;
+
+    public void MakeGroup()
+    {
+
+    }
+
+    public void JoinGroup()
+    {
+
+    }
+
+    public void Show()
+    {
+
+    }
+
+    public void Hide()
+    {
+
+    }
+}
