c31108630f49946f9b83accd49623b98b297bf85
Xiaoyi Zhang <517829767@qq.com>
2020-11-06 17:08:48 +0800

    no message

---
+++ Assets/DT/Scripts/UI/Talent/TalentItem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TalentItem : MonoBehaviour,IPointerEnterHandler,IPointerClickHandler
+{
+    public TalentItem prevItem;
+
+    public string talentDesc;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+
+    }
+}
---
+++ Assets/DT/Scripts/UI/Talent/TalentPanel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalentPanel : MonoBehaviour
+{
+    public List<TalentItem> dingWei = new List<TalentItem>();
+
+    public List<TalentItem> yeWuXiTong = new List<TalentItem>();
+
+    public List<TalentItem> guanJianZiYuanNengLi = new List<TalentItem>();
+
+    public List<TalentItem> yingLiMoShi = new List<TalentItem>();
+
+    public List<TalentItem> xianJinLiu = new List<TalentItem>();
+
+    public List<TalentItem> qiYeJiaZhi = new List<TalentItem>();
+
+
+
+    
+}
