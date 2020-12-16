df54ee08b1de4b883bd285b1ca474855caa57f89
wenchiet <no.none.non.null@gmail.com>
2020-12-16 18:05:00 +0800

    no message

--- Assets/DT/Scripts/NewFTE/FTE_1.5/FTE_1_5_Goal1.cs
+++ Assets/DT/Scripts/NewFTE/FTE_1.5/FTE_1_5_Goal1.cs
@@ -6,11 +6,33 @@ public class FTE_1_5_Goal1 : BaseGuideStep
 {
     public override IEnumerator StepStart()
     {
-        yield break;
+        InvokeRepeating("CheckGoal", 0, 1);
+        yield return new WaitForSeconds(0.5f);
     }
 
     public override IEnumerator StepEnd()
     {
+        CancelInvoke();
         yield break;
+    }
+
+    public override bool ChenkEnd()
+    {
+        return missiondatas.data[1].isFinish;
+    }
+
+    void CheckGoal()
+    {
+        missiondatas.data[0].currentNum = StageGoal.My.killNumber;
+        if (missiondatas.data[0].currentNum >= missiondatas.data[0].maxNum)
+        {
+            missiondatas.data[0].isFinish = true;
+        }
+
+        missiondatas.data[1].currentNum = StageGoal.My.totalCost;
+        if (missiondatas.data[1].currentNum >= missiondatas.data[1].maxNum)
+        {
+            missiondatas.data[1].isFinish = true;
+        }
     }
 }
---
+++ Assets/DT/Scripts/NewFTE/FTE_1.5/FTE_1_5_Goal2.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FTE_1_5_Goal2 : BaseGuideStep
+{
+    public Transform tapPanel;
+    public override IEnumerator StepStart()
+    {
+        InvokeRepeating("CheckGoal",0, 0.5f);
+        yield return new WaitForSeconds(0.5f);
+    }
+
+    public override IEnumerator StepEnd()
+    {
+        CancelInvoke();
+        yield break;
+    }
+
+    public override bool ChenkEnd()
+    {
+        
+        return missiondatas.data[0].isFinish &&missiondatas.data[1].isFinish&&missiondatas.data[2].isFinish;
+    }
+
+    void CheckGoal()
+    {
+        if (missiondatas.data[0].isFinish == false && RoleUpdateInfo.My.gameObject.activeInHierarchy)
+        {
+            missiondatas.data[0].isFinish = true;
+        }
+
+        if (missiondatas.data[1].isFinish == false && CreateTradeManager.My.gameObject.activeInHierarchy)
+        {
+            missiondatas.data[1].isFinish = true;
+        }
+
+        if (missiondatas.data[2].isFinish == false && tapPanel.childCount > 0)
+        {
+            missiondatas.data[2].isFinish = true;
+        }
+    }
+}
