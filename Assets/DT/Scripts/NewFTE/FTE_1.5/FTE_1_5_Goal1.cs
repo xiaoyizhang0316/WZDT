ac929a421d0724d463e53e59613e084bad6bb91e
wenchiet <no.none.non.null@gmail.com>
2021-01-11 12:59:14 +0800

    no message

--- Assets/DT/Scripts/NewFTE/FTE_1.5/FTE_1_5_Goal1.cs
+++ Assets/DT/Scripts/NewFTE/FTE_1.5/FTE_1_5_Goal1.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,19 +7,21 @@ public class FTE_1_5_Goal1 : BaseGuideStep
 {
     public GameObject tabPanel;
     public int costLimit;
-    public int finalCost = 0;
+    //public int finalCost = 0;
+    public int limitTime;
     public GameObject costPanel;
 
-    public int limitTime;
     private int currentTime = 0;
+    private int currentCost = 0;
     private bool fail;
     public GameObject bornPoint;
     public override IEnumerator StepStart()
     {
-        InvokeRepeating("CheckGoal", 0, 0.5f);
         currentTime = StageGoal.My.timeCount;
+        currentCost = StageGoal.My.totalCost;
         NewCanvasUI.My.GamePause(false);
-        costPanel.GetComponent<CostPanel>().InitCostPanel(0,currentTime);
+        costPanel.GetComponent<CostPanel>().InitCostPanel(currentCost,currentTime);
+        InvokeRepeating("CheckGoal", 0, 0.5f);
         SkipButton();
         yield return new WaitForSeconds(0.5f);
     }
@@ -53,9 +56,25 @@ public class FTE_1_5_Goal1 : BaseGuideStep
         }
     }
 
+    private bool isEnd = false;
     public override bool ChenkEnd()
     {
-        return missiondatas.data[0].isFail;
+        //return missiondatas.data[0].isFail;
+        if (isEnd)
+        {
+            FTE_1_5_Manager.My.goal1FinalCost = StageGoal.My.totalCost-currentCost;
+        }
+        return isEnd;
+    }
+
+    private void Reset()
+    {
+        CancelInvoke();
+        missiondatas.data[0].isFail = false;
+        currentTime = StageGoal.My.timeCount;
+        currentCost = StageGoal.My.totalCost;
+        costPanel.GetComponent<CostPanel>().InitCostPanel(currentCost,currentTime);
+        InvokeRepeating("CheckGoal", 0, 0.5f);
     }
 
     void CheckGoal()
@@ -63,27 +82,26 @@ public class FTE_1_5_Goal1 : BaseGuideStep
         if (StageGoal.My.timeCount - currentTime >= limitTime)
         {
             //fail = true;
+            HttpManager.My.ShowTip("超出时间限制，重置任务！");
             missiondatas.data[0].isFail = true;
             missiondatas.data[0].isFinish = false;
-            FTE_1_5_Manager.My.goal1FinalCost = StageGoal.My.totalCost;
+            //FTE_1_5_Manager.My.goal1FinalCost = StageGoal.My.totalCost;
+            Reset();
             return;
         }
-        costPanel.GetComponent<CostPanel>().ShowAllCost(StageGoal.My.totalCost,limitTime);
-        if (StageGoal.My.totalCost >= costLimit)
-        {
-            missiondatas.data[0].isFail = true;
-            missiondatas.data[0].isFinish = false;
-            FTE_1_5_Manager.My.goal1FinalCost = StageGoal.My.totalCost;
-            HttpManager.My.ShowTip("超出成本限制，任务失败！");
-            return;
-        }
+        costPanel.GetComponent<CostPanel>().ShowAllCost(StageGoal.My.totalCost-currentCost,limitTime);
         
         if (missiondatas.data[0].isFinish == false)
         {
             missiondatas.data[0].currentNum = StageGoal.My.killNumber;
             if (missiondatas.data[0].currentNum >= missiondatas.data[0].maxNum)
             {
-                missiondatas.data[0].isFinish = true;
+                if (StageGoal.My.totalCost - currentCost > costLimit)
+                {
+                    HttpManager.My.ShowTip("超出成本限制，本次任务失败！");
+                }
+                missiondatas.data[0].isFail = true;
+                isEnd = true;
             }
         }
     }
