94280b160375f5c59d0201bca855e9ac28fdf683
wenchiet <no.none.non.null@gmail.com>
2021-01-25 15:26:06 +0800

    no message

---
+++ Assets/DT/Scripts/NewFTE/FTE_1.5/video/TextScale.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+
+public class TextScale : MonoBehaviour
+{
+    private Vector3 origin_scale;
+    private void Start()
+    {
+        origin_scale = transform.localScale;
+    }
+
+    private void OnMouseOver()
+    {
+        Debug.LogWarning("in");
+        transform.DOScale(2, 0.2f).Play();
+    }
+
+    private void OnMouseExit()
+    {
+        Debug.LogWarning("out");
+
+        transform.DOScale(origin_scale, 0.2f).Play();
+    }
+}
