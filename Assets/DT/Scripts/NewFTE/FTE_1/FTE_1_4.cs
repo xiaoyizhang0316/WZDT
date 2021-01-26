f15ef01605af4f1bcd3fbe944d4e60a9e45acb36
Yseerd <Yseerd@outlook.com>
2021-01-26 14:10:18 +0800

    no message

---
+++ Assets/DT/Scripts/NewFTE/FTE_1/FTE_1_4.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+
+public class FTE_1_4 : BaseGuideStep
+{
+    // Start is called before the first frame update
+   
+    public override IEnumerator StepStart()
+    {
+       // NewCanvasUI.My.GamePause();
+        //RoleListManager.My.OutButton();
+
+     
+        //NewCanvasUI.My.Panel_Update.SetActive(true);
+        //RoleUpdateInfo.My.changeRoleButton.gameObject.SetActive(false);
+        //NewCanvasUI.My.Panel_Update.transform.localPosition = new Vector3(0,5000,0);
+        yield return new WaitForSeconds(0.5f);
+
+    }
+
+    public override IEnumerator StepEnd()
+    {
+        yield return new WaitForSeconds(0.5f);
+    }
+}
