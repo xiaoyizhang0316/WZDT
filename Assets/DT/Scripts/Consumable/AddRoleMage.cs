512fe0af6c6d9c4344b3c3e170ef68a8d3e09d47
Yseerd <Yseerd@outlook.com>
2021-02-20 13:54:00 +0800

    no message

---
+++ Assets/DT/Scripts/Consumable/AddRoleMage.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class AddRoleMage : BaseSpawnItem
+{
+  
+
+    public override void Init(int id)
+    {
+        StageGoal.My.GetTechPoint(40);
+        base.Init(id);
+  
+            Destroy(gameObject, 0.4f);
+    
+    }
+ 
+
+}
