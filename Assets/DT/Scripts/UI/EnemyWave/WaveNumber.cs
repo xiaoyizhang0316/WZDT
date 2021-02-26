25dee4a2b49d7874ee824e05ebc9bfba32271c1f
Xiaoyi Zhang <517829767@qq.com>
2021-02-26 13:38:56 +0800

    no message

---
+++ Assets/DT/Scripts/UI/EnemyWave/WaveNumber.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class WaveNumber : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler
+{
+
+    public int buildingNumber;
+
+    public void Init(int number)
+    {
+        buildingNumber = number;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        BuildingManager.My.GetBuildingByIndex(buildingNumber).StopShowPathLine();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        BuildingManager.My.GetBuildingByIndex(buildingNumber).ShowPathLine();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
