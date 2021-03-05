146e817a87c5272a282b4b683a2eb42d2588d5b7
Xiaoyi Zhang <517829767@qq.com>
2021-03-05 16:15:46 +0800

    添加版本号

--- Assets/DT/Scripts/ManMap/ToScene.cs
+++ Assets/DT/Scripts/ManMap/ToScene.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -11,6 +13,47 @@ public class ToScene : MonoBehaviour
     void Start()
     {
         GetComponent<Button>().onClick.AddListener(LoadScene);
+    }
+
+
+    public void SignOut()
+    {
+        StartCoroutine(Delete(() => {
+            SceneManager.LoadScene("Login");
+        }));
+        //SceneManager.LoadScene("Login");
+
+    }
+
+    public IEnumerator Delete(Action doend)
+    {
+        string path = Directory.GetParent( Directory.GetParent(Directory.GetParent(Application.dataPath).FullName).FullName) + "/Account.json";
+        if (File.Exists(path))
+        {
+            Debug.LogError("   存在 删除文件 ");
+
+            // System.IO.Directory.Delete(@updateAssets.list[0].LocalUrl);
+            try
+            {
+                File.Delete(path);
+            }
+            catch (Exception ex)
+            {
+                Debug.Log(" 删除文件时 出现错误 " + ex.Message);
+            }
+        }
+        else
+        {
+            doend();
+            yield break;
+        }
+
+        while (System.IO.File.Exists(path))
+        {
+            yield return null;
+        }
+
+        doend();
     }
 
     // Update is called once per frame
