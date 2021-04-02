3c5734d11f4e10b1ea292696747786b7f4f21e2b
Xiaoyi Zhang <517829767@qq.com>
2021-04-02 17:45:06 +0800

    Merge

--- Assets/DT/Scripts/OldFTE/TipsSign.cs
+++ Assets/DT/Scripts/OldFTE/TipsSign.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class TipsSign : MonoBehaviour
 {
     public GameObject target;
+
+    public int showTime = 30;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,14 +18,29 @@ public class TipsSign : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (GetComponentInParent<BaseGuideStep>().timeCount>1500)
-        {
-            transform.DOLocalMove(target.transform.localPosition,0.5f).Play();
-        }
+        //if (GetComponentInParent<BaseGuideStep>().timeCount>1500)
+        //{
+        //    transform.DOLocalMove(target.transform.localPosition,0.5f).Play();
+        //}
     }
 
     public void Check()
     {
        
+    }
+
+    public void Show()
+    {
+        transform.DOLocalMove(target.transform.localPosition, 0.5f).Play();
+    }
+
+    private void OnDisable()
+    {
+        CancelInvoke();
+    }
+
+    private void OnEnable()
+    {
+        Invoke("Show",showTime);
     }
 }
