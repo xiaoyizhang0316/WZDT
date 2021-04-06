84811fedabd60113b0586916f233bbce0b7d557e
wenchiet <no.none.non.null@gmail.com>
2021-04-06 18:36:39 +0800

    no message

--- Assets/DT/Scripts/NewMapFTE/FTE_1.5/HideRect.cs
+++ Assets/DT/Scripts/NewMapFTE/FTE_1.5/HideRect.cs
@@ -2,9 +2,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class HideRect : MonoBehaviour
+public class HideRect : MonoBehaviour, IPointerEnterHandler
 {
+    public bool needMouseOn = false;
+    [SerializeField]
+    private bool mouseOn = false;
     private void Start()
     {
         //Invoke("Hide", 2);
@@ -18,11 +23,37 @@ public class HideRect : MonoBehaviour
     private void OnEnable()
     {
         Debug.Log("enable exe");
-        Invoke("Hide", 2);
+        if (needMouseOn)
+        {
+            mouseOn = true;
+            GetComponent<Image>().raycastTarget = true;
+        }
+        
+        InvokeRepeating("Check", 0, 0.5f);
     }
+
+    void Check()
+    {
+        if (!mouseOn)
+        {
+            Invoke("Hide", 2);
+            CancelInvoke("Check");
+        }
+    }
+    
+    
 
     private void OnDisable()
     {
         CancelInvoke();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (needMouseOn)
+        {
+            mouseOn = false;
+            GetComponent<Image>().raycastTarget = false;
+        }
     }
 }
