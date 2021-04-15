9ecea8c8d808c5d4a9f2db18ca7eaccc7e071980
Yseerd <Yseerd@outlook.com>
2021-04-15 18:32:04 +0800

    no message

--- Assets/DT/Scripts/NewMapFTE/FTE_-2/T-2Manager/T_2Manager.cs
+++ Assets/DT/Scripts/NewMapFTE/FTE_-2/T-2Manager/T_2Manager.cs
@@ -7,12 +7,13 @@ public class T_2Manager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        StageGoal.My.maxRoleLevel = 3;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      TradeManager.My.HideAllIcon();
+      
     }
 }
