bc04c0cbf59497398907507098f9b789d7478001
Yseerd <Yseerd@outlook.com>
2021-04-27 17:54:02 +0800

    no message

---
+++ Assets/a/w.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class w : MonoBehaviour
+{
+    public Text te;
+    // Start is called before the first frame update
+    void Start()
+    {
+        te.DOText("100",3,true,ScrambleMode.Numerals);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
