9d83738d0d5ce747bb47e98c125e7816e4e0379b
Yseerd <Yseerd@outlook.com>
2021-05-07 16:12:31 +0800

    no message

---
+++ Assets/NewMap/indicatingSystem/IndicationSign.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class IndicationSign : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler
+{
+
+    public string describe;
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+       IndicatingManager.My.Init(transform,describe);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        IndicatingManager.My.Hide();
+    }
+}
