e9df45b9dc92997ad880b50cdaa3fcdc4dcb33da
wj_go <h_d_wj@126.com>
2021-05-10 16:28:08 +0800

    no message

---
+++ Assets/Old/Scripts/RoleManager/MapRole/Skill/SkillCheck/SkillCheckBase.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillCheckBase : MonoBehaviour
+{
+    public string target;
+    public string current;
+
+    public bool isPercent;
+
+    public string checkContent;
+
+    public BaseMapRole dependRole;
+
+    public bool isSuccess;
+
+    public Text contentText;
+    public Text currentText;
+    public int checkTurn;
+
+    protected virtual void InitCheck()
+    {
+        
+    }
+    
+    protected virtual void Check()
+    {
+        
+    }
+
+    public void ActiveCheck(BaseMapRole role, int checkTurn, CheckDetail detail)
+    {
+        dependRole = role;
+        target = detail.target;
+        isPercent = detail.isPersent;
+        checkContent = detail.checkContent;
+        contentText = transform.Find("contentText").GetComponent<Text>();
+        currentText = transform.Find("currentText").GetComponent<Text>();
+        contentText.text = checkContent;
+        this.checkTurn = checkTurn;
+
+        InitCheck();
+    }
+
+    public void ResetCheck()
+    {
+        CancelInvoke("Check");
+        isSuccess = false;
+        InitCheck();
+    }
+}
