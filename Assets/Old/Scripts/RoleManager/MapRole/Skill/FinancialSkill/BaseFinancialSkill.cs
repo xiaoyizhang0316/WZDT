5875f726c8c867be26c688a7e35f8e163da131f5
Xiaoyi Zhang <517829767@qq.com>
2021-05-24 19:08:31 +0800

    no message

--- Assets/Old/Scripts/RoleManager/MapRole/Skill/FinancialSkill/BaseFinancialSkill.cs
+++ Assets/Old/Scripts/RoleManager/MapRole/Skill/FinancialSkill/BaseFinancialSkill.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseFinancialSkill : MonoBehaviour
+public class BaseFinancialSkill : BaseSkill
 {
     public int index = -1;
     public virtual void Skill()
     {
         SkillCheckManager.My.ActiveRoleCheck(GetComponent<BaseMapRole>(),index);
+    }
+
+    public override void ReUnleashSkills()
+    {
+        
     }
 }
