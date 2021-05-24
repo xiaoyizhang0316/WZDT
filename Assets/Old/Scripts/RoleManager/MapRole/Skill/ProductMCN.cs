849dce409d4959392af508ea7d81309ae5db48ea
Xiaoyi Zhang <517829767@qq.com>
2021-05-24 15:44:24 +0800

    MCN修改激励等级技能

--- Assets/Old/Scripts/RoleManager/MapRole/Skill/ProductMCN.cs
+++ Assets/Old/Scripts/RoleManager/MapRole/Skill/ProductMCN.cs
@@ -7,10 +7,40 @@ public class ProductMCN : BaseProductSkill
     public int  addMoney;
 
     public int addScore;
+
+    public int normalEncourageId;
+
+    public int specialEncourageId;
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    /// <summary>
+    /// 根据消耗的产品切换激励等级技能
+    /// </summary>
+    /// <param name="type"></param>
+    public void ChangeEncourageSkill(int type)
+    {
+        if (role.encourageSkill.isSkillOpen)
+        {
+            role.encourageSkill.SkillOff();
+        }
+        if (type == 0)
+        {
+            role.encourageSkillId = normalEncourageId;
+            role.encourageSkill = null;
+            role.InitEncourageSkill();
+            role.encourageSkill.OnEncourageValueChange();
+        }
+        else
+        {
+            role.encourageSkillId = specialEncourageId;
+            role.encourageSkill = null;
+            role.InitEncourageSkill();
+            role.encourageSkill.OnEncourageValueChange();
+        }
     }
 
     public override void Skill()
